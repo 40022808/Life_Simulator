86c1dd35fbb8c994aabe78464ea5ab25816aa226
yehaoze <yehaozee@gmail.com>
2025-04-26 23:32:41 +0200

    0.0.6

--- src/事件.js
+++ src/事件.js
@@ -5,6 +5,7 @@ import { 人物属性 } from "./函数库.js";
 var 事件结算图片
 var 事件结算标题
 var 事件结算内容
+var 事件结算性别
 
 async function 事件结算获取函数(事件结算id) {
     try {
@@ -17,29 +18,35 @@ async function 事件结算获取函数(事件结算id) {
     }
 }
 
-export function 事件结算函数(事件结算id) {
+export function 事件结算函数(事件结算id,性别) {
     事件结算获取函数(事件结算id).then(result => {
         if (result) {
             事件结算图片 = result.图片;
             事件结算标题 = result.标题;
             事件结算内容 = result.内容;
+            事件结算性别 = result.性别;
         }
         else {
             console.log("未找到对应的数据");
         }
 
-        事件结算数据载入函数()
+        事件结算数据载入函数(性别)
     });
 }
 
 
-function 事件结算数据载入函数() {
+function 事件结算数据载入函数(性别) {
     const 游戏界面_事件_事件结算_图片 = document.getElementById("游戏界面_事件_事件结算_图片");
     const 游戏界面_事件_事件结算_事件_标题 = document.getElementById("游戏界面_事件_事件结算_事件_标题");
     const 游戏界面_事件_事件结算_事件_内容 = document.getElementById("游戏界面_事件_事件结算_事件_内容");
 
+    if (事件结算性别 == "有") {
+        游戏界面_事件_事件结算_图片.style.backgroundImage = "url(" + "assets/图片/" + 性别 + "/" + 事件结算图片 + ")";
+    }
+    else {
+        游戏界面_事件_事件结算_图片.style.backgroundImage = "url(" + "assets/图片/" + 事件结算图片 + ")";
+    }
 
-    游戏界面_事件_事件结算_图片.style.backgroundImage = "url(" + "assets/图片/" + 事件结算图片 + ")";
     游戏界面_事件_事件结算_事件_标题.innerHTML = 事件结算标题;
     游戏界面_事件_事件结算_事件_内容.innerHTML = 事件结算内容;
 }
